6e64fdc12ec141f4035a5f37ead8c4e7a300b103
Asman Bhusal <abhusal1@outlook.com>
2023-02-22 17:59:19 +0000

    Golang Simple Load Balancer

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server //this is interface
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
+// handles error once and we do not have to write this again and again
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func (s *simpleServer) Address() string { return s.addr }
+
+// to check if the server is alive or not
+func (s *simpleServer) IsAlive() bool { return true }
+
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+	return server
+}
+
+func (lb *LoadBalancer) serverProxy(rw http.ResponseWriter, req *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	fmt.Printf("Forwarding request to address %q\n", targetServer.Address())
+	targetServer.Serve(rw, req)
+}
+
+func main() {
+	// creating a slice or a list of servers where "Server" is an interface
+	servers := []Server{
+		newSimpleServer("https://www.facebook.com"),
+		newSimpleServer("http://www.bing.com"),
+		newSimpleServer("http://www.duckduckgo.com"),
+	}
+
+	lb := NewLoadBalancer("8000", servers)
+
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serverProxy(rw, req)
+	}
+
+	http.HandleFunc("/", handleRedirect)
+
+	fmt.Printf("Serving requests at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
+}
